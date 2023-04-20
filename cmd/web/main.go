dc9aa5f57e9a496c45dba6df4d8d9839074b0544
Gonzalo Garcia <gonzagarcia14@gmail.com>
2023-04-20 22:04:07 +0200

    Up to 4.7

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,42 +8,40 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	"snippetbox.gonzalogarcia.de/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP Network address")
-
-	dsn := flag.String("dsn", "web:Ambivalentb=4Antares@/snippetbox?parseTime=true", "MySQL Data Source Name")
-
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:Ambivalentb=4Antares@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-
 	db, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
-
 	defer db.Close()
-
+	// Initialize a models.SnippetModel instance and add it to the application
+	// dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
-
-	infoLog.Printf("Starting new server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
